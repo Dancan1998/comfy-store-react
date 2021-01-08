67e67c794765d7caf637ba2c8ae2896fd684a268
Dancan Chibole <dancankingstar@gmail.com>
2021-01-07 17:19:40 -0800

    first commit

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+const Error = () => {
+  return (
+    <div className="section section-center text-center">
+      <h2>there was an error</h2>
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import Error from "./ErrorPage";
+import PrivateRoute from "./PrivateRoute";
+export {
+  Home,
+  Products,
+  About,
+  SingleProduct,
+  Checkout,
+  Cart,
+  Error,
+  PrivateRoute,
+};
